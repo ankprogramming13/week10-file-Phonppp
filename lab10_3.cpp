25d44dbe237457ea991d70fcdefa51689881fc47
Unknown <kwangkg.30@gmail.com>
2018-01-26 14:27:04 +0700

    10_3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,4 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 int main() {
+        int cmd;
+        ofstream writeFile ("std.txt");
+        ifstream readFile ("std.txt");
+        string line;
+        string name;
+ cin >> cmd;
+
+        if (cmd = 1) {
+            cin >> name;
+            if (writeFile.is_open()) {
+                writeFile << name << endl;
+                writeFile.close();
+         }else{
+             cout << "Uanble to open file";
+         }
+        } else if (cmd = 2) {
+            if (readFile.is_open()) {
+                while (getline(readFile,line)) {
+                 cout << line << endl;
+                }
+                 }
+
+        }else{
+           return 0;
+        }
         /* code */
         return 0;
 }
